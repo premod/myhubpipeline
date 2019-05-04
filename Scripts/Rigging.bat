a75553f9a8518cc254f44981feeffac7f36d96e3
premod <premodmultimedia@gmail.com>
2019-05-04 10:41:20 +0530

    updated

--- Scripts/Rigging.bat
+++ Scripts/Rigging.bat
@@ -1,12 +1,7 @@
-
-
-
+copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2019"
+copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2018"
 copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2017"
-copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2016"
-
+copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2016.5"
 copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2015-x64"
-
 copy D:\Pipeline_TD_tools\myhubpipeline\Scripts\Rigging\ENV\maya.env "C:\Documents and Settings\%username%\My Documents\maya\2013-x64"
-
-
 net use D:\Pipeline_TD_tools\myhubpipeline\Scripts
